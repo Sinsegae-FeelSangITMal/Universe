655031a885be5c43ba50c96ce550cabf2ca05f0c
munjungmin <answjdals77@gmail.com>
2025-09-17 16:33:11 +0900

    [feat] User, Partner, PartnerUser 엔티티 정의

---
+++ src/main/java/com/sinse/universe/domain/Partner.java
@@ -0,0 +1,25 @@
+package com.sinse.universe.domain;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Entity
+@Data
+public class Partner {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "PT_ID")
+    private int id;
+
+    @Column(name = "PT_NM")
+    private String name;
+
+    @Column(name = "PT_ADR")
+    private String address;
+
+    @Column(name = "PT_FIL")
+    private String filePath;
+}
+
+
