c7751b0259a6177bd221a1ffca2c767659527cd2
munjungmin <answjdals77@gmail.com>
2025-09-27 23:02:12 +0900

    [fix] oauth2 인증 성공 후 프론트의 /callback으로 리다이렉트

--- src/main/java/com/sinse/universe/model/auth/OAuth2FailureHandler.java
+++ src/main/java/com/sinse/universe/model/auth/OAuth2FailureHandler.java
@@ -1,12 +1,11 @@
 package com.sinse.universe.model.auth;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.sinse.universe.dto.response.ApiResponse;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.authentication.AuthenticationFailureHandler;
 import org.springframework.stereotype.Component;
@@ -18,24 +17,14 @@ import java.io.IOException;
 @RequiredArgsConstructor
 public class OAuth2FailureHandler implements AuthenticationFailureHandler {
 
-    private final ObjectMapper objectMapper;
+    @Value("${app.front-server.url}")
+    private String baseUrl;
 
     @Override
     public void onAuthenticationFailure(HttpServletRequest request, HttpServletResponse response, AuthenticationException exception) throws IOException, ServletException {
         log.error("OAuth2 로그인 실패: {}", exception.getMessage());
 
-        // 프론트에 내려줄 응답 구성
-        ApiResponse<Object> apiResponse = ApiResponse.<Object>builder()
-                .success(false)
-                .message("OAuth 로그인에 실패했습니다. 다시 시도해주세요.") // 사용자 메시지
-                .code("OAUTH2_AUTHENTICATION_FAILED")                // 시스템 코드
-                .data(null)
-                .build();
-
-        response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
-        response.setCharacterEncoding("UTF-8");
-        response.setContentType("application/json;charset=UTF-8");
-        log.debug("fail response={}", objectMapper.writeValueAsString(apiResponse));
-        response.getWriter().write(objectMapper.writeValueAsString(apiResponse));
+        String redirectUrl = baseUrl + "/login?error=INVALID_CREDENTIALS";
+        response.sendRedirect(redirectUrl);
     }
 }
--- src/main/java/com/sinse/universe/model/auth/OAuth2SuccessHandler.java
+++ src/main/java/com/sinse/universe/model/auth/OAuth2SuccessHandler.java
@@ -1,7 +1,5 @@
 package com.sinse.universe.model.auth;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.sinse.universe.dto.response.ApiResponse;
 import com.sinse.universe.util.CookieUtil;
 import com.sinse.universe.util.JwtUtil;
 import jakarta.servlet.ServletException;
@@ -9,6 +7,7 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseCookie;
 import org.springframework.security.core.Authentication;
@@ -16,15 +15,16 @@ import org.springframework.security.web.authentication.SimpleUrlAuthenticationSu
 import org.springframework.stereotype.Component;
 
 import java.io.IOException;
-import java.util.Map;
 
 @Slf4j
 @Component
 @RequiredArgsConstructor
 public class OAuth2SuccessHandler extends SimpleUrlAuthenticationSuccessHandler {
 
+    @Value("${app.front-server.url}")
+    private String baseUrl;
+
     private final JwtUtil jwtUtil;
-    private final ObjectMapper objectMapper;
 
     @Override
     public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException, ServletException {
@@ -41,16 +41,9 @@ public class OAuth2SuccessHandler extends SimpleUrlAuthenticationSuccessHandler 
         ResponseCookie refreshCookie = CookieUtil.getResponseCookie(refreshToken, jwtUtil.getRefreshTokenTtl());
         response.addHeader(HttpHeaders.SET_COOKIE, refreshCookie.toString());
 
-        ApiResponse<Object> apiResponse = ApiResponse.<Object>builder()
-                .success(true)
-                .message("로그인에 성공했습니다.") // 사용자 메시지
-                .code("SUCCESS")                // 시스템 코드
-                .data(Map.of("accessToken", accessToken))
-                .build();
 
-        // 컨트롤러 메서드 진입 전에 핸들러가 호출되기 때문에 response body에 직접 write 해야함
-        response.setCharacterEncoding("UTF-8");
-        response.setContentType("application/json;charset-utf-8");
-        response.getWriter().write(objectMapper.writeValueAsString(apiResponse));
+        String redirectUrl = baseUrl + "/callback?accessToken=" + accessToken;
+        response.sendRedirect(redirectUrl);
+
     }
 }
